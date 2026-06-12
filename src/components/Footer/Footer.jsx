7db0c0b3a23b986da9c020b3b707b15d575a1961
Pablo Roda <pablo.roda@gmail.com>
2026-06-11 21:37:43 -0300

    Agregue el control de version

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 export const Footer = () => {
   return (
     <footer>
-      <p>Sitio desarrollado por pablo.roda@gmail.com</p>
+      <p>Sitio desarrollado por pablo.roda@gmail.com V2.0</p>
       <nav>
         <ul className="nav-list">
           <li></li>
